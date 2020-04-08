d0197352d85dc2aba1c73b3fa6164822175d88a9
Xiaojuan Wang <xiaojwan@nescafe.cs.washington.edu>
2020-04-07 23:49:48 -0700

    change crop size

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,5 +2,5 @@
 python main.py --batch_size 8 --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-4 --number_gpus 4 --gpu_ids 0 2 3 4 \
 --training_dataset Nba2k --training_dataset_dstype 'train'  \
 --validation_dataset Nba2k --validation_dataset_dstype 'val' \
---crop_size 1280 768 --batch_size 2 \
+--crop_size 768 1280 --batch_size 1 \
 --validation_frequency 10 --validation_n_batches 10 --render_validation --save_flow --inference_visualize
