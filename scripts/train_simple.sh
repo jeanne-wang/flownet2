954755777085abcfec2400604558a8327958f57e
Xiaojuan Wang <xiaojwan@Xiaojuans-MBP.attlocal.net>
2020-04-08 00:17:25 -0700

    change param

--- scripts/train_simple.sh
+++ scripts/train_simple.sh
@@ -1,7 +1,13 @@
 # Example on MPISintel Final and Clean, with L1Loss on FlowNet2 model
 python main.py --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-4 \
---number_gpus 4 --gpu_ids 0 2 3 4 \
---crop_size 768 1280 --batch_size 2 \
---training_dataset Nba2k --training_dataset_dstype 'train'  --training_dataset_img1_dirname '2k_mesh_rasterized' \
+--training_dataset Nba2k --training_dataset_dstype 'train' --training_dataset_img1_dirname '2k_mesh_rasterized' \
 --validation_dataset Nba2k --validation_dataset_dstype 'val' --validation_dataset_img1_dirname '2k_mesh_rasterized' \
---validation_frequency 10 --validation_n_batches 10 --render_validation --save_flow --inference_visualize
+--inference_dataset Nba2k --inference_dataset_dstype 'val' --inference_dataset_img1_dirname '2k_mesh_rasterized' \
+--crop_size 512 512 --batch_size 4 \
+--number_gpus 2 --gpu_ids 0 1 \
+--validation_frequency 10 \
+--render_validation --save_flow --inference_visualize \
+--inference_n_batches 40 --inference_batch_size 1 \
+--save './work_simple'
+
+
