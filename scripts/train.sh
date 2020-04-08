3dba744eddb890b4bd6b07b4b9dfa7442abea3ef
Xiaojuan Wang <xiaojwan@Xiaojuans-MBP.attlocal.net>
2020-04-07 23:16:21 -0700

    add options for savingf low

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,6 @@
 # Example on MPISintel Final and Clean, with L1Loss on FlowNet2 model
 python main.py --batch_size 8 --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-4 --number_gpus 4 --gpu_ids 0 2 3 4 \
 --training_dataset Nba2k --training_dataset_dstype 'train'  \
---validation_dataset Nba2k --validation_dataset_dstype 'val'
+--validation_dataset Nba2k --validation_dataset_dstype 'val' \
+--crop_size 1280 768 --batch_size 2 \
+--validation_frequency 10 --validation_n_batches 10 --render_validation --save_flow --inference_visualize
