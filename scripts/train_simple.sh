9b1f80cdcb9ed76beb33c5f781a390ba7243d154
Xiaojuan Wang <xiaojwan@ristretto.cs.washington.edu>
2020-04-09 16:16:56 -0700

    decrease lr

--- scripts/train_simple.sh
+++ scripts/train_simple.sh
@@ -1,5 +1,5 @@
 # Example on MPISintel Final and Clean, with L1Loss on FlowNet2 model
-python main.py --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-4 \
+python main.py --model FlowNet2 --loss=L1Loss --optimizer=Adam --optimizer_lr=1e-5 \
 --training_dataset Nba2k --training_dataset_dstype 'train' --training_dataset_img1_dirname '2k_mesh_rasterized' \
 --validation_dataset Nba2k --validation_dataset_dstype 'val' --validation_dataset_img1_dirname '2k_mesh_rasterized' \
 --inference_dataset Nba2k --inference_dataset_dstype 'val' --inference_dataset_img1_dirname '2k_mesh_rasterized' \
