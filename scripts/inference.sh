4e8d8d26c736a750a145657c863a815e52a18395
Xiaojuan Wang <xiaojwan@nescafe.cs.washington.edu>
2020-04-09 22:55:28 -0700

    change param

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,7 +1,7 @@
 # Example on MPISintel Final and Clean, with L1Loss on FlowNet2 model
-python main.py --inference --model FlowNet2 --save_flow \
---inference_dataset Nba2k --inference_dataset_dstype 'val' \
+python main.py --inference --model FlowNet2 --save_flow --inference_visualize \
+--inference_dataset Nba2k --inference_dataset_dstype 'train' \
 --number_gpus 1 --gpu_ids 0 \
 --resume './work/FlowNet2_model_best.pth.tar' \
 --inference_n_batches 10 --inference_batch_size 1 \
---save './val_results_game_to_rastered'
+--save './train_results_game_to_rastered'
